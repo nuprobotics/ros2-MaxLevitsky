5dba8f055b75aa47cd82a39f7789e52b8773cc23
MaxLevitsky <max.levitsky.5@gmail.com>
2026-02-16 16:44:31 +0200

    task 1

---
+++ Practice02/src/task01/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+package_name = 'task01'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/task1.launch']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'receiver = task01.receiver:main',
+        ],
+    },
+)
